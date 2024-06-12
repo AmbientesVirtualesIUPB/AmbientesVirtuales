6540f789b874d9811b03680c2556586169af8bcb
Morion <morion2013@gmail.com>
2024-06-12 17:59:20 -0500

    Corrección del código del botón (quitando dos líneas que no iban)

--- Assets/_VE/Scripts/UI/BtnColorPersonalizar.cs
+++ Assets/_VE/Scripts/UI/BtnColorPersonalizar.cs
@@ -34,12 +34,10 @@ public class BtnColorPersonalizar : MonoBehaviour
                 if (esPrincipal)
                 {
                     personalizacion.CambioColorPrincipal(indice);
-                    personalizacion.CambiarMaterialPiel(indice);
                 }
                 else
                 {
                     personalizacion.CambioColorSecundario(indice);
-                    personalizacion.CambiarMaterialPiel(indice);
                 }
                 break;
             case TipoElemento.piel:
