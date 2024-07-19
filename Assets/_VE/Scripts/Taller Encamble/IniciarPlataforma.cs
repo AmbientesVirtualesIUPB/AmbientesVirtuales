0d57ca2c62618088786f7fa8e137ec2477ae6d46
JulianMolinaJaramillo <judmolin@gmail.com>
2024-07-19 16:32:49 -0500

    Taller se ensamble 90% montado, con sus respectivas herramientas y assets, texturas de los furtivos, herramientas y personajes totalmente corregidas, se empieza a implementar la personalización de los furtivos para poder cambiar entre vehículos.

---
+++ Assets/_VE/Scripts/Taller Encamble/IniciarPlataforma.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
+
+public class IniciarPlataforma : MonoBehaviour
+{
+    public GameObject brazo; // El objeto a desplazar
+    public GameObject plataforma; // Referencia a las puertas de la plataforma para utilizar su componenete animator
+    public Transform[] posiciones; // El objeto al que quieres moverte
+    public float tiempoSuavizado = 0.3f; // Tiempo de suavizado
+    private Vector3 velocidad = Vector3.zero; // Vector de velocidad
+
+
+    /// <summary>
+    /// Funcion para iniciar la currutina del movimiento suave hacia arriba
+    /// </summary>
+    [ContextMenu("arriba")]
+    public void MoverArriba()
+    {
+        StopAllCoroutines();
+        StartCoroutine(MovimientoSuave(0));
+    }
+
+    /// <summary>
+    /// Funcion para iniciar la currutina del movimiento suave hacia abajo
+    /// </summary>
+    [ContextMenu("abajo")]
+    public void MoverAbajo()
+    {
+        StopAllCoroutines();
+        StartCoroutine(MovimientoSuave(1));
+    }
+
+
+    /// <summary>
+    /// Currutina para iniciar el movimiento suave
+    /// </summary>
+    /// <param name="posicion"> Indica a que posicon nos vamos a mover</param>
+    /// <returns></returns>
+    IEnumerator MovimientoSuave(int posicion)
+    {
+        // Validamos si se esta abriendo la compuerta
+        if (posicion == 0)
+        {   
+            // Activamos la animación deseada
+            plataforma.gameObject.GetComponent<Animator>().SetBool("open", true);
+            plataforma.gameObject.GetComponent<Animator>().SetBool("close", false);
+            // Damos una pequeña espera despues de abrir la compuerta
+            yield return new WaitForSeconds(1f);
+        }
+
+        // Mientras la distancia entre la posición actual y la posición objetivo sea mayor que una pequeña tolerancia
+        while (Vector3.Distance(transform.position, posiciones[posicion].position) > 0.001f)
+        {
+            // Aplica el movimiento suave
+            brazo.gameObject.transform.position = Vector3.SmoothDamp(brazo.gameObject.transform.position, posiciones[posicion].position, ref velocidad, tiempoSuavizado);
+
+            // Pausa el ciclo hasta el siguiente frame
+            yield return null;
+
+            // Validamos si se esta cerrando la compuerta y activamos la animacion y verificamos que tenga una pequeña tolerancia de distancia antes de cerrar
+            if (posicion == 1 && Vector3.Distance(brazo.gameObject.transform.position, posiciones[posicion].transform.position) < 0.5f)
+            {
+                plataforma.gameObject.GetComponent<Animator>().SetBool("open", false);
+                plataforma.gameObject.GetComponent<Animator>().SetBool("close", true);
+            }
+        }
+
+        // Una vez que se alcanza la posición objetivo, nos aseguramos de que el objeto esté exactamente en la posición objetivo
+        brazo.gameObject.transform.position = posiciones[posicion].position;
+    }  
+}
